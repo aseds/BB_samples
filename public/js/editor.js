0b683b838b9e0ded670472a285ea8768470958e1
aseds <aseds@localhost.localdomain>
2015-09-09 04:10:56 +0430

    how set view.el's id to be model's cid <div id='c1'...

--- public/js/editor.js
+++ public/js/editor.js
@@ -9,7 +9,7 @@
 	var RectangleView = Backbone.View.extend({
 		tagName: 'div',
 		className: 'rectangle',
-		id: function() {return this.model.cid},
+		id: function() {return this.model.cid}, // <div id="c1"...
 
 		events: {
 			'click': 'move'
