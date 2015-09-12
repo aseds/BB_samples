dd4b3cdcd6e5c8160e2137c418d2303bf7102407
aseds <aseds@localhost.localdomain>
2015-09-13 02:41:01 +0430

    model inherits from another model - 4

--- public/js/editor_models.js
+++ public/js/editor_models.js
@@ -1,29 +1,23 @@
-var A = Backbone.Model.extend({
-	initialize: function() {
-		console.log('initialize A');
-	},
-
-	asString: function () {
-		return JSON.stringify(this.toJSON());
+var Vehicle = Backbone.Model.extend({
+	dump: function() {
+		console.log(JSON.stringify(this.toJSON()));
 	}
 });
 
-var a = new A({
-	one: '1',
-	two: '2'
+var v = new Vehicle({type: 'car'});
+
+v.dump();
+// >> {"type":"car"}
+
+
+v.set('color', 'blue');
+v.dump();
+// >> {"type":"car","color":"blue"}
+
+v.set({
+	description: "<script>alert('script injection');</script>",
+	weight: 1750
 });
 
-// console.log(a.asString());
-
-var B = A.extend({});
-
-var b = new B({
-	three: '3'
-});
-
-// console.log(b.asString());
-
-console.log(b instanceof B);
-console.log(b instanceof A);
-console.log(b instanceof Backbone.Model);
-console.log(a instanceof B);
+v.dump()
+// >> {"type":"car","color":"blue","description""<script>alert("script injection");</script>","weight":1750}
